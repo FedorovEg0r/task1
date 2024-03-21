42d222cef0a6b887bbbc364f77cba92dc981f7dd
FedorovEg0r <94304007+FedorovEg0r@users.noreply.github.com>
2024-03-22 01:35:39 +0300

    Update settings.py

--- task/settings.py
+++ task/settings.py
@@ -128,9 +128,6 @@ CKEDITOR_UPLOAD_PATH = 'uploads/'
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
